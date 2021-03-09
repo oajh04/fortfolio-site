c327ec321721df22dbe61dc9dac2a33f7174a24e
oajh04 <cloudtubekr@naver.com>
2021-03-09 19:04:06 +0900

    app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Main from './Components/Main/Main';
+
+const App = () => {
+
+    return (
+        <>
+          <Main/>
+        </>
+    )
+}
+
+export default App;
