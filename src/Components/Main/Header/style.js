5ba3ae123ccf228cd347bfa89d1f1388df774343
oajh04 <cloudtubekr@naver.com>
2021-03-12 21:04:49 +0900

    style

---
+++ src/Components/Main/Header/style.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const canvasStyle = styled.canvas`
+    margin: 0px;
+    display: block;
+    width: 100vw;
+    height: 100vh;
+    background-color: #000000;
+    color: #ffffff;
+`;
+
+export const canvasText = styled.div`
+    color: white;
+`
