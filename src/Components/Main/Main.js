5e9a35d9fb2a6a8a93e38b871769b4910841a14b
oajh04 <cloudtubekr@naver.com>
2021-03-12 21:05:02 +0900

    Main

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,17 @@
+import Header from './Header/Header';
+import React from 'react';
+import Body from './Body/Body'
+import Footer from './Footer/Footer'
+
+const Main = () => {
+
+    return (
+        <>
+            <Header />
+            <Body />
+            <Footer />
+        </>
+    )
+}
+
+export default Main;
